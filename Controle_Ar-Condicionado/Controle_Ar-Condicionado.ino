6eb224e4e1080ae380632f07bf4bf2b4ca526556
william kleber <wkas@ic.ufal.br>
2019-02-04 17:16:33 -0200

    implementando parametros para o funcionamento
    Co-Authored-By: maxsuelrl <maxsuelrl@users.noreply.github.com>

--- Controle_Ar-Condicionado/Controle_Ar-Condicionado.ino
+++ Controle_Ar-Condicionado/Controle_Ar-Condicionado.ino
@@ -1,21 +1,44 @@
 #define pin1 7
-//#define pin2 6
+#define LIGADO 1
+#define DESLIGADO 0
 
+//status do arcondicionado
+int status = DESLIGADO;
+
+const int LM35 = A0; // Define o pino que lera a saída do LM35
+
+float temperatura; // Variável que armazenará a temperatura medida
 
 void setup() {
-  // put your setup code here, to run once:
+  
   pinMode( pin1, OUTPUT);
-//  pinMode( pin2, OUTPUT);
+  Serial.begin(9600); // inicializa a comunicação serial
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(digitalRead(pin1) == LOW)
+  temperatura = (float(analogRead(LM35))*5/(1023))/0.01;
+  Serial.print("Temperatura: ");
+
+  Serial.println(temperatura);
+
+  if(temperatura >= 30 && status == DESLIGADO)
   {
       digitalWrite(pin1, LOW);
       delay(1000);
       digitalWrite(pin1, HIGH);
       delay(1000);
+      status = LIGADO;
+
+  }
+
+  if(temperatura <= 23 && status == LIGADO)
+  {
+      digitalWrite(pin1, LOW);
+      delay(1000);
+      digitalWrite(pin1, HIGH);
+      delay(1000);
+      status = DESLIGADO;
 
   }
   
