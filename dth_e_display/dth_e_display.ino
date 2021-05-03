68554c991680b8670a0721c02210d3a5dad4248c
william kleber <wkas@ic.ufal.br>
2021-05-03 14:20:43 -0300

    ajustando diretórios

--- dth_e_display/dth_e_display.ino
+++ dth_e_display/dth_e_display.ino
@@ -3,6 +3,7 @@
 #include "DHT.h"
 
 #define Luz_Fundo  7
+#define pin1 1 //pino do rele
 #define DHTPIN A1 // pino que estamos conectado
 #define DHTTYPE DHT11 // DHT 11
  
@@ -15,47 +16,35 @@ DHT dht(DHTPIN, DHTTYPE);
  
 //cria um objeto tipo LiquidCrystal que chamei de "lcd" nos pinos citados:
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-char horas[4] = "tem";
-char minu[4];
-char temp[7]="00h00m";
 
-char* tempo(int segundos){
-  int h = 0;
-  int m = 0;
-  while(segundos >= 3600){
-    h =+ 1;
-    segundos -= 3600;
-  }
+int faltou_energia = true;
+int ligado = false;
 
-  while(segundos >= 60){
-    m =+ 1;
-    segundos -= 60;
-  }
+void comando_rele(){
+    digitalWrite (pin1, LOW);
+    delay(2000);
+    digitalWrite (pin1, HIGH);
 
-  sprintf(minu, "%d", m);
-  sprintf(horas, "%d", h);
-
-  temp[0] = horas[0];
-  temp[1] = horas[1];
-  temp[3] = minu[0];
-  temp[4] = minu[1];
-  Serial.println(temp);
-  return temp;
+    ligado = !ligado;
 }
+
+
 void setup()
 {
   lcd.begin(16, 2); // Iniciando o objeto "lcd" de 2 linhas e 16 colunas
   pinMode(Luz_Fundo,OUTPUT); //define o pino como saída
   digitalWrite(Luz_Fundo,HIGH); // Liga a luz do display.
+  pinMode(pin1,OUTPUT);
   Serial.begin(9600);
-  Serial.println("DHTxx test!");
-  
   dht.begin();
 }
 
 void loop()
 {
-
+  if(faltou_energia){
+    comando_rele();
+    faltou_energia = false;
+  }
    // O atraso do sensor pode chegar a 2 segundos.
   float h = dht.readHumidity();
   float t = dht.readTemperature();
@@ -63,22 +52,33 @@ void loop()
   if (isnan(t) || isnan(h))
   {
     Serial.println("Failed to read from DHT");
+    lcd.setCursor(0,0); // seta o cursor para: (coluna = 0, linha = 0)
+    lcd.print(" ERRO SENSOR");
   }
   else
   {
-    Serial.print("Umidade: ");
-    Serial.print(h);
-    Serial.print(" %t");
-    Serial.print("Temperatura: ");
-    Serial.print(t);
-    Serial.println(" *C");
+    if (t >= 30 && !ligado){
+      comando_rele();
+      delay(10000);
+    }
+    if (t <= 25 && ligado){
+      comando_rele();
+      delay(10000);
+    }
+    lcd.setCursor(0,0); // seta o cursor para: (coluna = 0, linha = 0)
+    lcd.print("TEMP: ");
+    lcd.print(t);
+    lcd.setCursor(0,1); // seta para linha 1, ou seja, a linha de baixo
+    lcd.print("");
+    lcd.print("HUM: ");
+    lcd.print(h);
+    lcd.print(" ");
+    if (ligado)
+      lcd.print("on");
+    else lcd.print("off");
   }
 
   
-  lcd.setCursor(0,0); // seta o cursor para: (coluna = 0, linha = 0)
-  lcd.print(" teste lcd");
-  lcd.setCursor(0,1); // seta para linha 1, ou seja, a linha de baixo
-  lcd.print("");
-  lcd.print(tempo(millis()/1000)); // escreve os segundos passados.
+  
   
 }
